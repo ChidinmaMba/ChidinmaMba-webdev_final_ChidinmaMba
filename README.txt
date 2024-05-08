a17d77a38c7dd0491a61aff2eff16e9080c3b522
ChidinmaMba <mbachidinma4@gmail.com>
2024-05-08 03:01:43 -0400

    finish frontend and backend

---
+++ README.txt
@@ -0,0 +1,12 @@
+Links to tutorials used:
+    https://www.youtube.com/watch?v=w3vs4a03y3I&t=35s
+    https://www.youtube.com/watch?v=bhiEJW5poHU&t=674s
+
+Tutorial Link:
+
+Things Learned:
+    - The MongoDB api for express
+    - Iterative building
+        - ensuring that at each milestone, the finished product is a usable product
+    
+
---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+const uri = 'mongodb+srv://mbachidinma4:8T8p8hqU8izlDPML@cluster0.eog0rot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+
+// app.get("/getEvents", (req, res)=>{
+//     console.log(req.query)
+//     res.json(
+//     {
+//         "events": [
+//         {
+//           "key": 1,
+//           "name": "Sour Mouse",
+//           "description": "Bar, board games, music, ping pong",
+//           "location": "ABC123 Seseme Street",
+//           "family_friendly": "false",
+//           "cost": "average drink cost 10 dollars",
+//           "start_time": "6pm",
+//           "end_time": "3am",
+//           "anytime": "false",
+//           "image_path": "images/bar.jpeg",
+//           "tags": []
+//         }
+//         ]
+//     })
+// })
+
+app.get("/getEvents", async (req, res) => {
+    console.log(req.query);
+    var tags = req.query.eventtags.split(" ");
+    let collection = await db.collection("events");
+    // let formattedTAgs = tags.map((tag)=>{
+    //     f`{tags:${tag}}`
+    // })
+    let query = " $or: [ { tags: \"bar\" }, { tags: \"music\" } ]"
+    // let newQuery = {tags:"bar"};
+    let newQuery = { tag: { $elemMatch: "bar" } };
+    let results = await collection.find({}).limit(50).toArray();
+    await console.log(results);
+    res.json(results).status(200);
+  });
+
+let conn;
+let db;
+function connectDB(){
+    try {
+        conn = mongoose.connect(uri);
+        db = mongoose.connection;
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+connectDB();
+
+app.listen(5000, () => {console.log("Server running on port 5000")})
