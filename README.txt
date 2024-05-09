031db71ecaf2289c28dd70b7cf90379e36b4cb88
ChidinmaMba <mbachidinma4@gmail.com>
2024-05-08 20:17:55 -0400

    add to README

--- README.txt
+++ README.txt
@@ -1,3 +1,12 @@
+Usage:
+BonBon is a website, specific to Manhattan, that gives you a curated list of fun things to do in the city
+based on your specific needs.
+
+The homepage has a list of some random places/events to go to.
+
+When you click "plan your day", you can select any combination of attributes to create a list of things to do in
+the city.
+
 Links to tutorials used:
     https://www.youtube.com/watch?v=w3vs4a03y3I&t=35s
     https://www.youtube.com/watch?v=bhiEJW5poHU&t=674s
@@ -8,5 +17,6 @@ Things Learned:
     - The MongoDB api for express
     - Iterative building
         - ensuring that at each milestone, the finished product is a usable product
+    - The difference between .find() and .aggregate()
     
 
--- server/server.js
+++ server/server.js
@@ -4,43 +4,21 @@ const app = express()
 const mongoose = require('mongoose')
 const uri = 'mongodb+srv://mbachidinma4:8T8p8hqU8izlDPML@cluster0.eog0rot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
-
-// app.get("/getEvents", (req, res)=>{
-//     console.log(req.query)
-//     res.json(
-//     {
-//         "events": [
-//         {
-//           "key": 1,
-//           "name": "Sour Mouse",
-//           "description": "Bar, board games, music, ping pong",
-//           "location": "ABC123 Seseme Street",
-//           "family_friendly": "false",
-//           "cost": "average drink cost 10 dollars",
-//           "start_time": "6pm",
-//           "end_time": "3am",
-//           "anytime": "false",
-//           "image_path": "images/bar.jpeg",
-//           "tags": []
-//         }
-//         ]
-//     })
-// })
-
 app.get("/getEvents", async (req, res) => {
     console.log(req.query);
     var tags = req.query.eventtags.split(" ");
     let collection = await db.collection("events");
-    // let formattedTAgs = tags.map((tag)=>{
-    //     f`{tags:${tag}}`
-    // })
-    let query = " $or: [ { tags: \"bar\" }, { tags: \"music\" } ]"
-    // let newQuery = {tags:"bar"};
-    let newQuery = { tag: { $elemMatch: "bar" } };
-    let results = await collection.find({}).toArray();
-    await console.log(results);
+    let query = {"tags": {"$all": tags}}
+    let results = await collection.aggregate([{"$match": query},{"$sample": { "size": 5 }}]).toArray();
+    console.log(results);
     res.json(results).status(200);
   });
+
+app.get("/getSomeEvents", async (req, res) => {
+    let collection = await db.collection("events");
+    let results = await collection.aggregate([{"$sample": { "size": 20 }}]).toArray();
+    res.json(results).status(200);
+})
 
 let conn;
 let db;
